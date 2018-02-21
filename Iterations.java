54fc9c7379edac3a59fdaa88ea41ee407ece11d5
SUNJE WINTELER - STUDENT <G00363332@gmit.ie>
2018-02-21 13:52:27 +0000

    Lab Wed Feb 21

---
+++ Iterations.java
@@ -0,0 +1,35 @@
+/*
+
+Name: Iterations
+
+Description: program which demonstrates what the given code will do
+
+Created by: Alice Winteler
+
+Created on: 21/02/2018
+
+*/
+
+public class Iterations
+{
+
+	public static void main(String[] args)
+	{
+		int i = 1;
+		int j = 2;
+		int k = 3;
+		int m = 2;
+
+		for(i = 1; i <= 5; i++){
+			for(j = 1; j <= 3; j++){
+				for(k = 1; k <= 4; k++)
+					System.out.print('*');
+
+				System.out.println();
+			}
+
+			System.out.println();
+		}
+	}
+
+}
